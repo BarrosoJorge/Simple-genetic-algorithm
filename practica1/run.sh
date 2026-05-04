dbc22ffaa4a12324a4476ed0b9ee7e4f54f57136
Hector Emilio Valdes Velazquez <141884263+Emilio-Velazquez@users.noreply.github.com>
2026-05-03 21:11:13 -0600

    Comment out third function call in run.sh

--- practica1/run.sh
+++ practica1/run.sh
@@ -11,3 +11,4 @@ cmake ..
 cmake --build .
 
 ./programa -gaussianas 1 -sup 5 1 0.3 -inf 0 0 0.05 -poblacion 10 -generaciones 1 -cruza 0.85 -mutacion 0.02 -bits 8
+#./programa -gaussianas 5 -sup 2 1 0.5 -inf -2 0 0.01 -poblacion 500 -generaciones 2000 -cruza 0.85 -mutacion 0.02 -bits 12 #Tercera función
