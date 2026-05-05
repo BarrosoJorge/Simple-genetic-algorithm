7728869a1180dbb100abf5199353cea960f2d58d
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2026-05-04 21:18:41 -0600

    the pipeline is ready

--- practica1/run.sh
+++ practica1/run.sh
@@ -11,10 +11,10 @@ cmake ..
 cmake --build .
 
 # PRIMERA FUNCION (DataSet a) #
-# ./programa -gaussianas 1 -sup 5 1 0.3 -inf 0 0 0.05 -poblacion 10 -generaciones 1 -cruza 0.85 -mutacion 0.02 -bits 8
+#./programa -gaussianas 5 -sup 3 1 0.3 -inf -2 0 0.05 -poblacion 500 -generaciones 2000 -cruza 0.8 -mutacion 0.01 -bits 20
 
 # SEGUNDA FUNCION (DataSet b) #
-./programa -gaussianas 10 -sup 3 1 1 -inf -3 0 0.01 -poblacion 500 -generaciones 2000 -cruza 0.85 -mutacion 0.08 -bits 16
+#./programa -gaussianas 10 -sup 3 1 1 -inf -3 0 0.01 -poblacion 500 -generaciones 2000 -cruza 0.85 -mutacion 0.08 -bits 16
 
 # TERCERA FUNCION (DataSet c) #
-#./programa -gaussianas 5 -sup 2 1 0.5 -inf -2 0 0.01 -poblacion 500 -generaciones 2000 -cruza 0.85 -mutacion 0.02 -bits 12
+./programa -gaussianas 5 -sup 2 1 0.5 -inf -2 0 0.01 -poblacion 500 -generaciones 2000 -cruza 0.85 -mutacion 0.02 -bits 12
